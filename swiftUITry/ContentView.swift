09bc5fb0b90f9cf6c177cad3188e92338efdcf10
aiQG <940163124@qq.com>
2019-10-28 10:15:06 +0800

    Initial Commit

---
+++ swiftUITry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUITry
+//
+//  Created by 周测 on 10/28/19.
+//  Copyright © 2019 aiQG_. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
