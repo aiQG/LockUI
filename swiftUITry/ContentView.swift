963da84924daf996fda16cad6edb75c2a6f3b474
aiQG <940163124@qq.com>
2019-10-28 16:52:54 +0800

    底层颜色

--- swiftUITry/ContentView.swift
+++ swiftUITry/ContentView.swift
@@ -9,8 +9,15 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	
     var body: some View {
-        Text("Hello World")
+		VStack {
+			Color.gray.edgesIgnoringSafeArea(.all)
+			Text("aaaaaaaaaaaaaaaaaaaaaaa")
+				.background(Color.red)
+			LockedScene()
+		}
     }
 }
 
---
+++ swiftUITry/LockedScene.swift
@@ -0,0 +1,49 @@
+//
+//  LockedScene.swift
+//  swiftUITry
+//
+//  Created by 周测 on 10/28/19.
+//  Copyright © 2019 aiQG_. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct LockedScene: View {
+	@State var isLocked: Bool = true
+	let colors = Gradient(colors: [.green, .init(Color.RGBColorSpace.sRGB, red: 0, green: 1, blue: 0, opacity: 0)])
+	var body: some View {
+		ZStack {
+			Color.gray.edgesIgnoringSafeArea(.all) //底层灰色
+			//to unlock scene
+			Circle()
+				.fill(RadialGradient(gradient: colors, center: .center, startRadius: 40, endRadius: 50))//渐变填充
+				.opacity(isLocked ? 0 : 1)
+				.scaleEffect(isLocked ? 0 : 10)
+				.animation(.easeOut(duration: 1.5))
+			//icon
+			VStack {
+				if isLocked {
+					Image(systemName: "lock")
+						.scaleEffect(2)
+						.animation(.default)
+					//.offset(y: 150)
+				} else {
+					Image(systemName: "lock.open")
+						.scaleEffect(2)
+						.animation(.default)
+				}
+				Button(action: {self.isLocked.toggle()}) {
+					Text("Open")
+				}
+			}
+			
+		}
+	}
+}
+
+struct LockedScene_Previews: PreviewProvider {
+	static var previews: some View {
+		LockedScene()
+	}
+}
